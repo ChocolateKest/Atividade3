8aac387b7c780a261584b1ce2ab9044f9d8211b3
ChocolateKest <gustavoemartins@gmail.com>
2025-05-20 20:47:13 -0300

    commit

---
+++ src/main/java/com/mycompany/atividade3/Atividade3.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.atividade3;
+
+/**
+ *
+ * @author gusta
+ */
+public class Atividade3 {
+
+    public static void main(String[] args) {
+       int i = 14;
+       do {
+           System.out.println(i);
+          i = i + 2;  
+       }while(i<23);
+        
+    }
+}
